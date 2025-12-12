4aecaee0896edf365222130b135beaf0f03c10cd
Pavlo Muravel <luis.proskuriv@gmail.com>
2025-12-13 00:29:08 +0100

    add modalku

---
+++ assets/js/abaut_us.js
@@ -0,0 +1,42 @@
+
+
+const modalSubscribe = document.getElementsByClassName('modal_subsckribe')[0];
+const modalSend = document.getElementsByClassName('modal_send')[0];
+const openModalSubscribe = document.getElementById('subscribe');
+const openModalSend = document.getElementById('send');
+const closeModal = document.getElementsByClassName('modal__content-clous');
+
+
+openModalSubscribe.addEventListener('click', (e) => {
+  modalSubscribe.style.display = 'flex';
+})
+
+openModalSend.addEventListener('click', (e) => {
+  modalSend.style.display = 'flex';
+})
+
+
+for (let btn of closeModal) {
+  btn.addEventListener('click', (e) => {
+    modalSubscribe.style.display = 'none';
+    modalSend.style.display = 'none';
+  })
+}
+
+window.addEventListener('click', function (event)  {
+  if (event.target === modalSubscribe) {
+    modalSubscribe.style.display = 'none';
+  }
+  if (event.target === modalSend) {
+    modalSend.style.display = 'none';
+  }
+})
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    modalSubscribe.style.display = 'none';
+    modalSend.style.display = 'none';
+  }
+
+})
+
